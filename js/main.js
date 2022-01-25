40525491bd468f26c5463e3f6b5deac467307393
david83748 <david.vashchenko@gmail.com>
2022-01-25 14:16:18 +0200

    add linter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
 
-fetch('http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no')
+fetch('https://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no')
     .then((response) =>  response.json())
     .then((data) => render(data.location, data.current))
 
