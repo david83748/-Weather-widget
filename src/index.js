3b0a25560f4d599404d6638a2cb5a00d1992cfc5
david83748 <david.vashchenko@gmail.com>
2022-02-07 21:33:04 +0200

    add destra...

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import render from './render';
 
 fetch('https://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no')
   .then((response) => response.json())
-  .then((data) => render(data.location, data.current));
+  .then((data) => render(data));
